48997bdcb2b820569e076f0fd8a09354e4437ad6
nolen22 <nolenperrino@gmail.com>
2025-03-29 21:05:09 -0400

    fix: update Next.js configuration with all domains

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
+    domains: [
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com',
+      'twitdupe.vercel.app',
+      'twitdupe-nolen22s-projects.vercel.app',
+      'twitdupe-q4um8purc-nolen22s-projects.vercel.app'
+    ],
   },
+  output: 'standalone',
+  poweredByHeader: false,
 };
 
 module.exports = nextConfig; 
